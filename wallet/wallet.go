051312f280960b6b584a6a79a6bc46879bdfb693
bbboyjan <bbboyjan@outlook.com>
2020-11-03 17:50:59 +0800

    wallet need password

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"encoding/hex"
+	"github.com/tybc/errors"
 	"golang.org/x/crypto/ed25519"
 )
 
@@ -10,7 +11,11 @@ type Wallet struct {
 	Pk  ed25519.PrivateKey
 }
 
-func My() *Wallet {
+func My(password string) (*Wallet, error) {
+	if len(password) == 0 {
+		return nil, errors.New("invalid wallet password")
+	}
+
 	//hard code key
 	pub, _ := hex.DecodeString("b12049d709358dc427433050625aa2135163181ccc320f22859d7c065ecc9dcb")
 	pk, _ := hex.DecodeString("b4af5bb2bb1fb9086d2cac65a667f1810dfb0ddd904f2edc947227271fdcaba5b12049d709358dc427433050625aa2135163181ccc320f22859d7c065ecc9dcb")
@@ -18,5 +23,5 @@ func My() *Wallet {
 	return &Wallet{
 		Pub: pub,
 		Pk:  pk,
-	}
+	}, nil
 }
